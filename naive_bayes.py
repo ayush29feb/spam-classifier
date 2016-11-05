cf23663b54fa3a48c01258fc19381c68c037becc
Ayush Saraf <ayush29f@attu4.cs.washington.edu>
2016-11-05 16:21:39 -0700

    tabs -> spaces

--- naive_bayes.py
+++ naive_bayes.py
@@ -6,22 +6,24 @@ SPAM_TRAIN_DIR = 'data/train/spam/'
 # This function reads in a file and returns a 
 # set of all the tokens. It ignores the subject line
 def token_set(filename):
-	#open the file handle
-	with open(filename, 'r') as f:
-		#ignore the Subject beginning
-		text = f.read()[9:]
-		#put it all on one line
-		text = text.replace('\r', '')
-		text = text.replace('\n', ' ')
-		#split by spaces
-		tokens = text.split(' ')
-		#return the set of unique tokens
-		return set(tokens)
+  #open the file handle
+  with open(filename, 'r') as f:
+    #ignore the Subject beginning
+    text = f.read()[9:]
+    #put it all on one line
+    text = text.replace('\r', '')
+    text = text.replace('\n', ' ')
+    #split by spaces
+    tokens = text.split(' ')
+    #return the set of unique tokens
+    return set(tokens)
 
 def p_ham_spam():
   ham = len(os.listdir(HAM_TRAIN_DIR)) * 1.0
   spam = len(os.listdir(SPAM_TRAIN_DIR)) * 1.0
   return ham / (ham + spam), spam / (ham + spam)
+
+def p_x_spam(token):
 
 p_ham, p_spam = p_ham_spam()
 
