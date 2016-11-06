5eb1a3f881832683b5c9956cb9a7d81b0711fa80
Ayush Saraf <ayush29f@attu4.cs.washington.edu>
2016-11-05 18:09:20 -0700

    using dp

--- naive_bayes.py
+++ naive_bayes.py
@@ -32,8 +32,12 @@ def read_emails(dirname):
       ret[token] = 1 + ret[token] if token in ret else 1
   return ret
 
-def p_w(word, words_count, email_count):
-  return (1.0 + (0 if word not in words_count else words_count[word])) / (2.0 + email_count)
+def p_w(word, words_count, email_count, mem):
+  if word in mem:
+    return mem[word]
+  ret = (1.0 + (0 if word not in words_count else words_count[word])) / (2.0 + email_count)
+  mem[word] = ret
+  return ret
 
 def classify(filename):
   ham_count = len(os.listdir(HAM_TRAIN_DIR)) * 1.0
@@ -41,12 +45,15 @@ def classify(filename):
   ham_words = read_emails(HAM_TRAIN_DIR)
   spam_words = read_emails(SPAM_TRAIN_DIR)
   
+  ham_p_mem = {}
+  spam_p_mem = {}
+
   tokens = token_set(filename)
   prod_tokens_ham = math.log(ham_count / (ham_count + spam_count))
   prod_tokens_spam = math.log(spam_count / (ham_count + spam_count))
   for token in tokens:
-    prod_tokens_ham += math.log(p_w(token, ham_words, ham_count))
-    prod_tokens_spam += math.log(p_w(token, spam_words, spam_count))
+    prod_tokens_ham += math.log(p_w(token, ham_words, ham_count, ham_p_mem))
+    prod_tokens_spam += math.log(p_w(token, spam_words, spam_count, spam_p_mem))
 
   return prod_tokens_spam > (math.log(9) + prod_tokens_ham)
 
